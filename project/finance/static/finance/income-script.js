43738898e80506c00534370e764a84668c1bc7c2
Sebastian Pabst <83839857+sebseb0802@users.noreply.github.com>
2025-08-06 14:04:16 +0000

    feat: add inheritance to finance pages and style to expenses page
    Add inheritance to finance pages such that they now inherit from
    'finance-base.html'. Add styling to expenses page consistent with income
    page.

--- project/finance/static/finance/income-script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("open-sidebar").onclick = function() {
-        document.getElementById("sidebar").classList.add("open");
-    };
-    document.getElementById("close-sidebar").onclick = function() {
-        document.getElementById("sidebar").classList.remove("open");
-    };
-});
